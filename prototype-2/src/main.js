df2222d85127124d2e1919b3d760114705627195
Stefan B <Stefan.bie@live.de>
2017-01-19 12:36:44 +0100

    - disable scroll

--- prototype-2/src/main.js
+++ prototype-2/src/main.js
@@ -34,6 +34,40 @@ canvas.customAttributes.arc = function (xloc, yloc, value, total, R) {
 canvas.customAttributes.after = function(){
 
 };
+var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+
+function preventDefault(e) {
+  e = e || window.event;
+  if (e.preventDefault)
+    e.preventDefault();
+  e.returnValue = false;
+}
+
+function preventDefaultForScrollKeys(e) {
+  if (keys[e.keyCode]) {
+    preventDefault(e);
+    return false;
+  }
+}
+
+function disableScroll() {
+  if (window.addEventListener) // older FF
+    window.addEventListener('DOMMouseScroll', preventDefault, false);
+  window.onwheel = preventDefault; // modern standard
+  window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
+  window.ontouchmove  = preventDefault; // mobile
+  document.onkeydown  = preventDefaultForScrollKeys;
+}
+
+function enableScroll() {
+  if (window.removeEventListener)
+    window.removeEventListener('DOMMouseScroll', preventDefault, false);
+  window.onmousewheel = document.onmousewheel = null;
+  window.onwheel = null;
+  window.ontouchmove = null;
+  document.onkeydown = null;
+}
+disableScroll();
 
 Raphael.st.draggable = function() {
   var me = this,
@@ -245,7 +279,7 @@ function createButton(src, start_circle, color) {
       console.error("Wrong state")
     }
   });
-  group.draggable()
+  group.draggable();
 
   return group;
 }
